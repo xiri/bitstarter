e9b677d07fe956255ccdb1e1692e4e974d791c0c
xiri <xiri@xiris-MacBook-Pro.local>
2013-07-05 17:59:31 +0700

    made web.js read index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var buf = fs.readFileSync( __dirname + '/index.html' )
+  response.setHeader( 'Content-Type', 'text/html' );  
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
